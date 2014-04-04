b3aa9c6acd27fba93c7f7186b0a21bae92362aa9
Tomasz Stanislawski <t.stanislaws@samsung.com>
2014-04-04 16:53:18 +0200

    clk: propagate parent change up one level
    This patch adds support for propagation of setup of clock's parent one level
    up.
    
    This feature is helpful when a driver changes topology of its clocks using
    clk_set_parent().  The problem occurs when on one platform/SoC driver's clock
    is located at MUX output but on the other platform/SoC there is a gated proxy
    clock between the MUX and driver's clock.  In such a case, driver's code has to
    be modified to use one clock for enabling and the other clock for setup of a
    parent.
    
    The code updates are avoided by propagating setup of a parent up one level.
    
    Additionally, this patch adds CLK_SET_PARENT_PARENT (sorry for naming) flag to
    inform clk-core that clk_set_parent() should be propagated.
    
    Signed-off-by: Tomasz Stanislawski <t.stanislaws@samsung.com>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -30,6 +30,7 @@
 #define CLK_GET_RATE_NOCACHE	BIT(6) /* do not use the cached clk rate */
 #define CLK_SET_RATE_NO_REPARENT BIT(7) /* don't re-parent on rate change */
 #define CLK_GET_ACCURACY_NOCACHE BIT(8) /* do not use the cached clk accuracy */
+#define CLK_SET_PARENT_PARENT	BIT(9) /* propagate parent change up one level */
 
 struct clk_hw;
 struct dentry;
